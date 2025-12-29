3ebfb8465575764e81f4e6b6a937b072feedaf4d
Aaryan Degama <aaryandegama@gmail.com>
2025-12-29 14:05:16 +0530

    I will burn down git

--- src/data.py
+++ src/data.py
@@ -1,5 +1,9 @@
 import os
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> 6b5a171 (Implemented the init function 🦆🦆)
 Ding_dir = ".ding"
 
 def init():
@@ -7,8 +11,16 @@ def init():
     ding_path = os.path.join(cwd, Ding_dir)
 
     if os.path.exists(ding_path):
+<<<<<<< HEAD
         print("It is already a ding repository")
         return
 
     os.mkdir(ding_path)
     print(f"Initialisied a ding repo in {ding_path}")
+=======
+        print("It is a ding repository")
+        return
+
+    os.mkdir(ding_path)
+    print(f"Initialisied an ding repo in {ding_path}")
+>>>>>>> 6b5a171 (Implemented the init function 🦆🦆)
