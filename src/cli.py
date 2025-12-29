0562b2155c9e44725b61192af24e798acb1fab30
Aryan <ether9095cable@gmail.com>
2025-12-29 22:14:52 +0530

    Resolving conflicts

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,15 @@
 import argparse
 from src import data
 
+
 def init(args):
     data.init()
 
-def parse_args():
-    parser= argparse.ArgumentParser(prog="ding")
 
-    commands= parser.add_subparsers(dest="command", required=True)
+def parse_args():
+    parser = argparse.ArgumentParser(prog="ding")
+
+    commands = parser.add_subparsers(dest="command", required=True)
 
     init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
