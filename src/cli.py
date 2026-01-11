55bae7cc29d47a6a6abbbc2aaf4d87e25b14a035
deepali <deepali_24a12res222@iitp.ac.in>
2026-01-11 13:22:47 +0530

    Setup project and update cli output

--- src/cli.py
+++ src/cli.py
@@ -50,8 +50,9 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
-    args.func(args)
+    print("Ding Dong, who's there?")
+    print("The IRS")
+    print("Oh Shit")
 
 
 if __name__ == "__main__":
